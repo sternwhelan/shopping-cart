83d646c2dd872ddd158b056803ca764aa0e26927
sternwhelan <rw2362@stern.nyu.edu>
2021-06-10 17:17:26 -0400

    Updated for office hours

--- shopping_cart.py
+++ shopping_cart.py
@@ -25,26 +25,26 @@ products = [
 
 
 def to_usd(my_price):
-    """
-    Converts a numeric value to usd-formatted string, for printing and display purposes.
+    
+    #Converts a numeric value to usd-formatted string, for printing and display purposes.
 
-    Param: my_price (int or float) like 4000.444444
+    #Param: my_price (int or float) like 4000.444444
 
-    Example: to_usd(4000.444444)
+    #Example: to_usd(4000.444444)
 
-    Returns: $4,000.44
-    """
+    #Returns: $4,000.44
+    
     return f"${my_price:,.2f}" #> $12,000.71
 
 
 # TODO: write some Python code here to produce the desired output
 
-print(len(products))
+#print(len(products))
 
 
 # capture product ids until we're done (use infinite while loop)
 
-selected_ids - []
+selected_ids = []
 
 
 while True:
