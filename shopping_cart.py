4600851288565d133c28920987598b0bd41cdd8a
sternwhelan <rw2362@stern.nyu.edu>
2021-06-11 10:11:51 -0400

    Separate Capture and display

--- shopping_cart.py
+++ shopping_cart.py
@@ -42,25 +42,30 @@ def to_usd(my_price):
 #INFORMATION CAPTURE / INPUTS
 #
 total_price = 0
+selected_ids = []
+
 
 while True:
 
 
-    selected_id = input("Please put in a product ID or enter 'DONE': ")
+    selected_id = input("Please put in a product ID or enter 'DONE' if there are no more items: ")
 
     if selected_id == "DONE":
         break
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] +" " + str(matching_product["price"]))
-
+        selected_ids.append(selected_id)
 
 #INFO AND DISPLAY OUTPUT FOR TOTAL PRICE
 
+#print(selected_ids)
 
-print("TOTAL PRICE: ") + str(total_price))
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] +" " + str(matching_product["price"]))
+
+print("TOTAL PRICE: " + str(total_price))
 
 #print(len(products))
 
